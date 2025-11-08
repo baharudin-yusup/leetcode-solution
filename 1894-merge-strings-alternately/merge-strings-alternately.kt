511e3b38724e3e2fdb53ff81db12e5006d8b67d0
Baharudin Yusup <baharudin.yusup@outlook.com>
2025-11-08 15:16:49 +0700

    Time: 92 ms (76.80%) | Memory: 40.4 MB (76.13%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.kt
+++ 1894-merge-strings-alternately/merge-strings-alternately.kt
@@ -1,36 +1,20 @@
 class Solution {
     fun mergeAlternately(word1: String, word2: String): String {
-        var i = 0
-        var j = 0
         val word1Length = word1.length
         val word2Length = word2.length
 
-        val charArr = CharArray(word1Length + word2Length) { currentIndex ->
-            when {
-                i < word1Length && j < word2Length -> {
-                    if (currentIndex % 2 == 0) {
-                        val currentChar = word1[i]
-                        i++
-                        currentChar
-                    } else {
-                        val currentChar = word2[j]
-                        j++
-                        currentChar
-                    }
-                }
-                i < word1Length -> {
-                    val currentChar = word1[i]
-                    i++
-                    currentChar
-                }
-                else -> {
-                    val currentChar = word2[j]
-                    j++
-                    currentChar
-                }
+        val sb = StringBuilder()
+
+        for (i in 0 until max(word1Length, word2Length)) {
+            if (i < word1Length) {
+                sb.append(word1[i])
+            }
+
+            if (i < word2Length) {
+                sb.append(word2[i])
             }
         }
 
-        return String(charArr)
+        return String(sb)
     }
 }

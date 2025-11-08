af5477a7501beff6fccea28868352cc7b4a05198
Baharudin Yusup <baharudin.yusup@outlook.com>
2025-11-08 15:23:03 +0700

    Time: 94 ms (71.60%) | Memory: 40.7 MB (55.47%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.kt
+++ 1894-merge-strings-alternately/merge-strings-alternately.kt
@@ -2,18 +2,24 @@ class Solution {
     fun mergeAlternately(word1: String, word2: String): String {
         val word1Length = word1.length
         val word2Length = word2.length
-
+        var i = 0
         val sb = StringBuilder()
 
-        for (i in 0 until max(word1Length, word2Length)) {
-            if (i < word1Length) {
-                sb.append(word1[i])
-            }
-
-            if (i < word2Length) {
-                sb.append(word2[i])
-            }
+        while (i < min(word1Length, word2Length)) {
+            sb.append(word1[i])
+            sb.append(word2[i])
+            i++
         }
+
+        while (i < word1Length) {
+            sb.append(word1[i])
+            i++
+        }
+
+        while (i < word2Length) {
+            sb.append(word2[i])
+            i++
+        } 
 
         return String(sb)
     }

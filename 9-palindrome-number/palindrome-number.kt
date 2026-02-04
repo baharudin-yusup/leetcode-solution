f67e1b53706fbb8e49e24ce01de7d51c3553761f
Baharudin Yusup <baharudin.yusup@outlook.com>
2026-02-04 18:19:22 +0700

    Time: 5 ms (99.50%) | Memory: 45.4 MB (93.71%) - LeetSync

--- 9-palindrome-number/palindrome-number.kt
+++ 9-palindrome-number/palindrome-number.kt
@@ -1,17 +1,16 @@
 class Solution {
     fun isPalindrome(x: Int): Boolean {
-        if (x < 0) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false
         }
 
         var tempX = x
         var reverseX = 0
-        // 102 ==> 1x10^2 + 0x10^1 + 2x10^0
-        // 2 == 102 % 10 == 10x10 + 2
-        // 20
-        // 201
-        // 201 ==> 2x10^2 + 0x10^1 + 1x10^0
 
+        // 102 ==> 1*10^2 + 0*10^1 + 2*10^0
+        // 2 == 0*10 + 102%10
+        // 20 == 2*10 + 10%10
+        // 201 == 20*10 + 1%10
         while (tempX > 0) {
             reverseX *= 10
             reverseX += tempX % 10

ce4203a5dddd3f9a28354e23e8e8b156f70afb01
Baharudin Yusup <baharudin.yusup@outlook.com>
2026-02-24 12:30:38 +0700

    Time: 35 ms (32.57%) | Memory: 52.7 MB (18.86%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.kt
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.kt
@@ -1,25 +1,11 @@
 class Solution {
     fun findDifference(nums1: IntArray, nums2: IntArray): List<List<Int>> {
-        val map1 = nums1.associate { it to true }.toMutableMap()
-        val map2 = nums2.associate { it to true }.toMutableMap()
+        val set1 = nums1.toSet()
+        val set2 = nums2.toSet()
 
-        nums1.forEach { num ->
-            map2[num] = false
-        }
-
-        nums2.forEach { num ->
-            map1[num] = false
-        }
-
-        val distinct1 = map1
-            .filterValues { it }
-            .keys
-            .toList()
-        val distinct2 = map2
-            .filterValues { it }
-            .keys
-            .toList()
-        
-        return listOf(distinct1, distinct2)
+        return listOf(
+            set1.subtract(set2).toList(),
+            set2.subtract(set1).toList()
+        )
     }
 }

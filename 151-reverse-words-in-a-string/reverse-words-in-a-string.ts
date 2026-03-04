7864dce084496b913c6d4ef2d3b1689dfb120c12
Baharudin Yusup <baharudin.yusup@outlook.com>
2026-03-04 17:51:40 +0700

    Time: 0 ms (100.00%) | Memory: 57.3 MB (75.97%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.ts
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.ts
@@ -2,6 +2,6 @@ function reverseWords(s: string): string {
     return s.trim()
         .split(" ")
         .filter((s) => s !== "")
-        .reduce((acc, curr) => `${curr} ${acc}`, "")
-        .trim();
+        .reverse()
+        .join(" ");
 };

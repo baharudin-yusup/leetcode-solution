fc0878e19c6b5231bf3b3432be91cb70555ceacf
Baharudin Yusup <baharudin.yusup@outlook.com>
2026-03-18 17:21:45 +0700

    Time: 6 ms (100.00%) | Memory: 62.8 MB (79.83%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.ts
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.ts
@@ -1,60 +1,33 @@
-const areSetsEqual = <T>(setA: Set<T>, setB: Set<T>): boolean => {
-  // First, check if the sizes are the same. If not, they are not equal.
-  if (setA.size !== setB.size) {
-    return false;
-  }
-
-  // Iterate over the elements of the first set
-  for (const value of setA) {
-    // Check if the second set has every value from the first set
-    if (!setB.has(value)) {
-      return false;
-    }
-  }
-
-  // If all elements match and sizes are equal, the sets are equal
-  return true;
-};
 
 function closeStrings(word1: string, word2: string): boolean {
     if (word1.length != word2.length) {
         return false;
     }
 
-    let word1Set = new Set(word1);
-    let word2Set = new Set(word2);
-
-    if (!(areSetsEqual(word1Set, word2Set))) {
-        return false;
+    if (word1 === word2) {
+        return true;
     }
 
-    // {a: 3, b: 5, c: 3, ...}
-    let word1Map = new Map<string, number>();
-    let word2Map = new Map<string, number>();
+    const w1Array = new Array(26).fill(0);
+    const w2Array = new Array(26).fill(0);
 
-    
+
     for (let i = 0; i < word1.length; i++) {
-        const w1 = word1[i];
-        const w2 = word2[i];
-        word1Map.set(w1, (word1Map.get(w1) ?? 0) + 1);
-        word2Map.set(w2, (word2Map.get(w2) ?? 0) + 1);
+        w1Array[word1.charCodeAt(i) - 97]++;
+        w2Array[word2.charCodeAt(i) - 97]++;
     }
 
-    // {1: 2, 4: 2, 3: 2, ...}
-    let word1MapLength = new Map<number, number>();
-    let word2MapLength = new Map<number, number>();
+    for (let i = 0; i < 26; i++) {
+        if ((!w1Array[i] && w2Array[i]) || (w1Array[i] && !w2Array[i])) {
+            return false;
+        }
+    }
 
-    word1Map.forEach((value, _) => {
-        const prevValue = word1MapLength.get(value) ?? 0;
-        word1MapLength.set(value, prevValue + 1);
-    });
+    w1Array.sort();
+    w2Array.sort();
 
-    word2Map.forEach((value, _) => {
-        word2MapLength.set(value, (word2MapLength.get(value) ?? 0) + 1);
-    });
-
-    for (const [key, value] of word1MapLength) {
-        if (value != word2MapLength.get(key)) {
+    for (let i = 0; i < 26; i++) {
+        if (w1Array[i] != w2Array[i]) {
             return false;
         }
     }

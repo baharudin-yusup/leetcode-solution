4cc8c94c0939c0a5faf8d49cf3162d73c984e8b7
Baharudin Yusup <baharudin.yusup@outlook.com>
2026-03-18 17:25:08 +0700

    Time: 12 ms (97.48%) | Memory: 62.6 MB (84.87%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.ts
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.ts
@@ -7,25 +7,30 @@ function closeStrings(word1: string, word2: string): boolean {
     if (word1 === word2) {
         return true;
     }
-
+    
+    // Map total alphabet, index-0 = total a
     const w1Array = new Array(26).fill(0);
     const w2Array = new Array(26).fill(0);
-
 
     for (let i = 0; i < word1.length; i++) {
         w1Array[word1.charCodeAt(i) - 97]++;
         w2Array[word2.charCodeAt(i) - 97]++;
     }
 
+    // Check whether all alphabet is available on both word
+    // Related to operation 1
     for (let i = 0; i < 26; i++) {
         if ((!w1Array[i] && w2Array[i]) || (w1Array[i] && !w2Array[i])) {
             return false;
         }
     }
 
+    // Sort the quantity
     w1Array.sort();
     w2Array.sort();
 
+    // Check whether all quantity is same on both word
+    // Related to operation 2
     for (let i = 0; i < 26; i++) {
         if (w1Array[i] != w2Array[i]) {
             return false;

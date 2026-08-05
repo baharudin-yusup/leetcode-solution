db6645c6eb1fe54217d485de7bf72ec761db1a4e
Baharudin Yusup <baharudin.yusup@outlook.com>
2026-08-05 18:29:51 +0700

    Time: 55 ms (18.04%) | Memory: 54.1 MB (5.00%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.kt
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.kt
@@ -4,26 +4,30 @@ class Solution {
             return 0
         }
 
-        var longestSubstring = 0
-        
-        for (i in 0..<s.length) {
-            val charMap = mutableMapOf<Char, Boolean>()
-            val currentString = s.substring(i)
-            var currentTotal = 0
-            for (c in currentString) {
-                if (charMap[c] == null) {
-                    charMap[c] = true
-                    currentTotal++
-                } else {
-                    break
-                }
+        var maxLength = 0
+
+        var leftPointer = 0
+        val charMap = mutableMapOf<Char, Int>()
+
+        for ((i, char) in s.withIndex()) {
+            val currentCharIndex = charMap[char]
+            charMap[char] = i
+
+            // println(">>> [${i}] Current char = ${char} | currentCharIndex = ${currentCharIndex}")
+            if (currentCharIndex != null && currentCharIndex + 1 > leftPointer) {
+                leftPointer = currentCharIndex + 1
+                // println(">>> Change left pointer to = ${leftPointer}")
             }
 
-            if (currentTotal > longestSubstring) {
-                longestSubstring = currentTotal
+            // println(">>> Current left pointer = ${leftPointer} | current righ pointer = ${i}")
+
+            val currentLength = i - leftPointer + 1
+            if (currentLength > maxLength) {
+                maxLength = currentLength
+                // println("${s.substring(leftPointer, i + 1)} | length = ${s.substring(leftPointer, i + 1).length} | max-length = ${maxLength}")
             }
         }
-        
-        return longestSubstring
+
+        return maxLength
     }
 }

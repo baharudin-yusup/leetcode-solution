efafd3ce3339faebbd40708213b5f7de2423dc63
Baharudin Yusup <baharudin.yusup@outlook.com>
2026-09-03 11:35:50 +0700

    Time: 21 ms (33.63%) | Memory: 46.8 MB (16.09%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.kt
+++ 20-valid-parentheses/valid-parentheses.kt
@@ -1,29 +1,15 @@
 class Solution {
     fun isValid(s: String): Boolean {
+        val pairs = mapOf(')' to '(', '}' to '{', ']' to '[')
         val stack = ArrayDeque<Char>()
 
         for (char in s) {
-            // println(">>> currentChar: ${char}")
             when (char) {
-                '(', '{', '[' -> {
-                    // println("Add to stack")
-                    stack.addLast(char)
-                    // println("new stack: ${stack}")
-                }
-                else -> {
-                    val correctOpeningBracket = if (char == ')') '('
-                        else if (char == '}') '{' else '['
-                    // println("correctOpeningBracket: ${correctOpeningBracket}")
-                    val last = stack.lastOrNull() ?: return false
-                    if (last == correctOpeningBracket) {
-                        stack.removeLastOrNull()
-                    } else {
-                        return false
-                    }
-                }
+                '(', '{', '[' -> stack.addLast(char)
+                else -> if (stack.removeLastOrNull() != pairs[char]) return false
             }
         }
 
-        return stack.size == 0
+        return stack.isEmpty()
     }
 }

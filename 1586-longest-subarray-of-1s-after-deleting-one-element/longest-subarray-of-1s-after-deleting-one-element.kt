d8c948d63150458d89fbdaf6911c15af29a66a4d
Baharudin Yusup <baharudin.yusup@outlook.com>
2026-09-07 15:56:47 +0700

    Time: 3 ms (96.36%) | Memory: 58.7 MB (95.45%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.kt
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.kt
@@ -1,28 +1,49 @@
 class Solution {
+    // fun longestSubarray(nums: IntArray): Int {
+    //     var l = -1
+    //     var totalZero = 0
+    //     var max = 0
+    //     var curr = 0
+        
+    //     for (i in 0 until nums.size) {
+    //         if (nums[i] > 0) {
+    //             curr++
+    //         } else {
+    //             totalZero++
+    //         }
+
+    //         while (totalZero > 1 && l < nums.size) {
+    //             l++
+    //             if (nums[l] == 0) {
+    //                 totalZero--
+    //             } else {
+    //                 curr--
+    //             }
+    //         }
+
+    //         max = maxOf(curr, max)
+    //     }
+
+    //     return if (max == nums.size) max - 1 else max
+    // }
+
     fun longestSubarray(nums: IntArray): Int {
-        var l = -1
-        var totalZero = 0
+        var prevPath = 0
+        var currPath = 0
         var max = 0
-        var curr = 0
         
         for (i in 0 until nums.size) {
-            if (nums[i] > 0) {
-                curr++
+            if (nums[i] == 1) {
+                currPath++
             } else {
-                totalZero++
+                max = maxOf(max, prevPath + currPath)
+                prevPath = currPath
+                currPath = 0
             }
-
-            while (totalZero > 1 && l < nums.size) {
-                l++
-                if (nums[l] == 0) {
-                    totalZero--
-                } else {
-                    curr--
-                }
-            }
-
-            max = maxOf(curr, max)
         }
+
+        // Handle when last index is not `0`
+        max = maxOf(max, prevPath + currPath)
 
         return if (max == nums.size) max - 1 else max
     }

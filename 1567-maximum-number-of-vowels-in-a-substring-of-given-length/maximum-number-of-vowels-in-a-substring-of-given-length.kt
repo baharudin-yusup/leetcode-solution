188f2813e784856aa152f1598bd5abfbf74c3158
Baharudin Yusup <baharudin.yusup@outlook.com>
2026-09-07 15:39:23 +0700

    Time: 19 ms (83.85%) | Memory: 47 MB (81.37%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.kt
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.kt
@@ -2,30 +2,30 @@ class Solution {
     // s = "leetcode", k=3
     fun maxVowels(s: String, k: Int): Int {
         var curr = 0
-        var max = 0
 
         // Initial value
         // 'l', 'e', 'e'
         for (i in 0 until k) {
-            if (s[i].isVowel()) curr++
+            if (s[i].isVowel) curr++
         }
         
         // curr = 2
         // max = 2
-        max = curr
+        var max = curr
         //  0    1    2    3    4    5    6    7
         // 'l', 'e', 'e', 't', 'c', 'o', 'd', 'e'
         // 't', 'c', 'o', 'd', 'e'
         // i = 3, 4, 5, 6, 7 (i == last char in this window)
         for (i in k until s.length) {
-            if (s[i - k].isVowel()) curr--
-            if (s[i].isVowel()) curr++
+            if (s[i - k].isVowel) curr--
+            if (s[i].isVowel) curr++
             max = maxOf(curr, max)
         }
 
         return max
     }
 
-    private val vowels = setOf('a', 'i', 'u', 'e', 'o')
-    private fun Char.isVowel() = vowels.contains(this)
+    private val Char.isVowel
+        get() = this == 'a' || this == 'i' || this == 'u' || this == 'e' || this == 'o'
+
 }

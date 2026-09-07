b860636cfe978fb0732565ba9c49de2596d66425
Baharudin Yusup <baharudin.yusup@outlook.com>
2026-09-07 15:35:32 +0700

    Time: 43 ms (29.19%) | Memory: 47.7 MB (54.66%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.kt
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.kt
@@ -1,28 +1,31 @@
 class Solution {
-    private val Char.isVowel
-        get() = this == 'a' || this == 'e' || this == 'i' || this == 'o' || this == 'u'
-
+    // s = "leetcode", k=3
     fun maxVowels(s: String, k: Int): Int {
-        var currentTotalVowel = 0
+        var curr = 0
+        var max = 0
+
+        // Initial value
+        // 'l', 'e', 'e'
         for (i in 0 until k) {
-            if (s[i].isVowel) {
-                currentTotalVowel++
-            }
+            if (s[i].isVowel()) curr++
         }
-
-        if (k == s.length) {
-            return currentTotalVowel
-        }
-
-        var maxTotalVowel = currentTotalVowel
-
+        
+        // curr = 2
+        // max = 2
+        max = curr
+        //  0    1    2    3    4    5    6    7
+        // 'l', 'e', 'e', 't', 'c', 'o', 'd', 'e'
+        // 't', 'c', 'o', 'd', 'e'
+        // i = 3, 4, 5, 6, 7 (i == last char in this window)
         for (i in k until s.length) {
-            val prevCounter = if (s[i - k].isVowel) -1 else 0
-            var nextCounter = if (s[i].isVowel) 1 else 0
-            currentTotalVowel += prevCounter + nextCounter
-            maxTotalVowel = max(maxTotalVowel, currentTotalVowel)
+            if (s[i - k].isVowel()) curr--
+            if (s[i].isVowel()) curr++
+            max = maxOf(curr, max)
         }
 
-        return maxTotalVowel
+        return max
     }
+
+    private val vowels = setOf('a', 'i', 'u', 'e', 'o')
+    private fun Char.isVowel() = vowels.contains(this)
 }

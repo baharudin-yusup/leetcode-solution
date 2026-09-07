dd135911343246412d976fdcef68d9dea19fa26d
Baharudin Yusup <baharudin.yusup@outlook.com>
2026-09-07 15:57:32 +0700

    Time: 2 ms (100.00%) | Memory: 60.2 MB (70.91%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.kt
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.kt
@@ -1,32 +1,4 @@
 class Solution {
-    // fun longestSubarray(nums: IntArray): Int {
-    //     var l = -1
-    //     var totalZero = 0
-    //     var max = 0
-    //     var curr = 0
-        
-    //     for (i in 0 until nums.size) {
-    //         if (nums[i] > 0) {
-    //             curr++
-    //         } else {
-    //             totalZero++
-    //         }
-
-    //         while (totalZero > 1 && l < nums.size) {
-    //             l++
-    //             if (nums[l] == 0) {
-    //                 totalZero--
-    //             } else {
-    //                 curr--
-    //             }
-    //         }
-
-    //         max = maxOf(curr, max)
-    //     }
-
-    //     return if (max == nums.size) max - 1 else max
-    // }
-
     fun longestSubarray(nums: IntArray): Int {
         var prevPath = 0
         var currPath = 0
@@ -45,6 +17,7 @@ class Solution {
         // Handle when last index is not `0`
         max = maxOf(max, prevPath + currPath)
 
+        // if `max == nums.size` then it's mean all the array item is `1` 
         return if (max == nums.size) max - 1 else max
     }
 }
